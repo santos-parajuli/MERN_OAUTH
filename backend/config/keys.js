e4671b6e8fe52888550f34e34098170929101f2f
Santosh Parajuli <49607455+santos-parajuli@users.noreply.github.com>
2020-11-06 12:42:11 +0545

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,17 +1,17 @@
 module.exports = {
   Google : {
-    GOOGLE_CLIENT_ID:"848109427768-07922480e55s4v5no58avduu3and33j7.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET:"RWPP3JZbx2J53JbEL3xy2yzk",
+    GOOGLE_CLIENT_ID:"Your google client id",
+    GOOGLE_CLIENT_SECRET:"your google client secret",
 		CALLBACK_URL: "http://localhost:5000/api/auth/google/callback"
   },
   Facebook:{
-    APP_ID:"313676810052865",
-    APP_SECRET:"fae191baa37153ce8d83e14c1ca8a5d4",
+    APP_ID:"Your facebook app id",
+    APP_SECRET:"your facebook app secret",
     callbackURL: "http://localhost:5000/api/auth/facebook/callback"
   },
-  mongoURI: "mongodb+srv://santos_parajuli:sasi987@nodejs.uhjwr.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret",
-  SESSION_SECRET:"verysecret",
+  mongoURI: "Your mongo uri",
+  secretOrKey: "your secret",
+  SESSION_SECRET:"your session secret",
   FACEBOOK_APP_ID:"",
   FACEBOOK_APP_SECRET:"",
 };
